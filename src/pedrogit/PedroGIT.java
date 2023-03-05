365faa42c78ba8ceb3a99283ee84719678f844ad
Pedro <Pedro@DESKTOP-GC15A4U>
2023-03-05 17:27:32 +0100

    Commit 01: int = 1

---
+++ src/pedrogit/PedroGIT.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pedrogit;
+
+/**
+ *
+ * @author Pedro
+ */
+public class PedroGIT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Pedro Marques Garcia
+        System.out.println("Programa Principal");
+        int valor = 1;
+    }
+    
+}
